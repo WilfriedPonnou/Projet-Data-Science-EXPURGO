f4ec34fd0ba5fc7e07ff17aecd46fba144739c93
vooloohoo <69146981+vooloohoo@users.noreply.github.com>
2021-06-30 10:14:41 +0200

    Update streamlit_webapp.py

--- streamlit_webapp.py
+++ streamlit_webapp.py
@@ -12,12 +12,13 @@ from streamlit_folium import folium_static
 from folium.plugins import MarkerCluster
 from geopy.geocoders import Nominatim
 from datetime import datetime
+from streamlit_echarts import st_echarts
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-st.set_page_config(page_title="Expurgo", page_icon="https://image.flaticon.com/icons/png/512/2640/2640438.png")
+st.set_page_config(page_title="Expurgo", page_icon="https://www.camping-croisee-chemins.fr/wp-content/uploads/2021/02/Recyclage.png")
 
-file = './map_data.csv'
+file = './data/map_data.csv'
 
 locator = Nominatim(user_agent="myGeocoder")
 
@@ -44,7 +45,8 @@ def get_address(lat,lon):
 
 model = load_model()
 
-a = st.sidebar.radio('Navigation:',["photo","carte"])
+st.sidebar.subheader("Bienvenue")
+a = st.sidebar.radio('Navigation:',["photo","carte","tableau de bord"])
 
 if a == "photo":
     st.title("uploader une photo ")
@@ -65,6 +67,7 @@ if a == "photo":
             final_pred = st.text_input('Entrez la bonne catégorie')
         else:
             final_pred = pred
+        num = st.number_input('Enter a number', min_value=0,value=1)
 
         "votre choix est : ", final_pred
 
@@ -94,7 +97,8 @@ if a == "photo":
                 'category' : [final_pred],
                 'lat' : [result['GET_LOCATION']['lat']],
                 'lon' : [result['GET_LOCATION']['lon']],
-                'date' : date
+                'date' : date,
+                'number' : num
             })
             new_data = pd.concat([new_data,address],axis=1)
             map_data = pd.read_csv(file, index_col=0)
@@ -109,12 +113,12 @@ if a == "photo":
                 [result['GET_LOCATION']['lat'], result['GET_LOCATION']['lon']], popup=str(final_pred), tooltip=tooltip
             ).add_to(m)
             folium_static(m)
+            st.subheader("merci, le déchet a été ajouté à la carte")
 
 
 if a == "carte":
     st.title("cartographie des déchets ")
     map_data = pd.read_csv(file, index_col=0)
-    map_data
 
     m = folium.Map(location=[46.232192999999995,2.209666999999996], zoom_start=6)
     map_data = pd.read_csv(file, delimiter=",")
@@ -126,3 +130,83 @@ if a == "carte":
             icon=folium.Icon(color="red", icon="trash",prefix="fa"),
         ).add_to(marker_cluster)
     folium_static(m)
+
+if a == "tableau de bord":
+    st.title("tableau de bord")
+
+    data = pd.read_csv(file, index_col=0)
+
+    st.sidebar.subheader('Paramètres tableau de bord:')
+
+    city_list = ['all cities']+data.groupby("town").agg('sum').index.tolist()
+    selected_city = st.sidebar.selectbox('Select your city :', city_list)
+    if selected_city != 'all cities':
+        data2 = data[data['town']==selected_city]
+        data_per_classes = data2.groupby("category").agg('sum')
+    else:
+        data_per_classes = data.groupby("category").agg('sum')
+
+    waste_list = ['all waste']+data.groupby("category").agg('sum').index.tolist()
+    selected_waste = st.sidebar.selectbox('Select a waste :', waste_list)
+    if selected_waste != 'all waste':
+        data2 = data[data['category']==selected_waste]
+        data_per_city = data2.groupby("town").agg('sum')
+    else:
+        data_per_city = data.groupby("town").agg('sum')
+
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        xAxis = data_per_city.index.tolist()
+        yAxis = data_per_city["number"].tolist()
+        xAxis = [x for y, x in sorted(zip(yAxis, xAxis),reverse=True)]
+        yAxis.sort(reverse=True)
+
+        st.subheader("répartition des déchets par ville :")
+        options = {
+            "dataZoom": [
+                    {
+                        "type": 'slider',
+                        "start": 0,
+                        "end": 50
+                    }
+                ],
+            "xAxis": {
+                "type": "category",
+                "data": xAxis
+            },
+            "yAxis": {"type": "value"},
+            "series": [{"data": yAxis, "type": "bar"}],
+        }
+        st_echarts(options=options)
+
+    with col2:
+        st.subheader("classification des déchets par type :")
+
+        d = []
+        for i in range(len(data_per_classes['number'])):
+            d.append(dict(value=data_per_classes['number'].iloc[i], name=data_per_classes.index[i]))
+
+        options = {
+            "tooltip": {"trigger": "item"},
+            "legend": {"left": "center"},
+            "series": [
+                {
+                    "name": "nombre de déchet :",
+                    "type": "pie",
+                    "radius": ["40%", "70%"],
+                    "avoidLabelOverlap": False,
+                    "itemStyle": {
+                        "borderRadius": 10,
+                        "borderColor": "#fff",
+                        "borderWidth": 2,
+                    },
+                    "label": {"show": False, "position": "center"},
+                    "emphasis": {
+                        "label": {"show": True, "fontSize": "20", "fontWeight": "bold"}
+                    },
+                    "labelLine": {"show": False},
+                    "data": d,
+                }
+            ],
+        }
+        st_echarts(options=options)
